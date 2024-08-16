98b212c75f0f97cf3b754680f1e3afc3495afb28
primera7790 <def.primera@gmail.com>
2024-08-16 19:18:36 +0500

    added frontend

--- src/pages/router.py
+++ src/pages/router.py
@@ -12,8 +12,3 @@ templates = Jinja2Templates(directory='templates')
 @router.get('/')
 def get_main_page(request: Request):
     return templates.TemplateResponse('base.html', {'request': request})
-
-
-@router.get('/predict')
-def get_predict_page(request: Request):
-    return templates.TemplateResponse('base.html', {'request': request})
